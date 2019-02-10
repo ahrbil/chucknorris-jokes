64d5db17c66b6ec066b97ebe91cc8f8bddbed2cc
ahrbil <ahrbil.me@gmail.com>
2019-02-10 14:08:25 +0100

    Running prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <h1>Chucknorris Jokes</h1>
-    );
-  }
-}
+const App = () => <h1>Chucknorris Jokes</h1>;
 
 export default App;
