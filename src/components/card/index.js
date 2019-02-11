0ae83f0fcac79a3c4e8d15d0f831384f383b99a8
ahrbil <ahrbil.me@gmail.com>
2019-02-11 17:15:11 +0100

    Updating joke card to work with detach component

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Detach from "../detach";
 
+// we wrap our joke card in a Detach component
+// so we can render it differently when we click it
+// when we click any card we will render it alone on screen
+// and when we click on the overlay we go back to our feed
 const JokeCard = ({ joke }) => (
-  <JokeCardStyle>
-    <p>{joke}</p>
-  </JokeCardStyle>
+  <Detach>
+    <JokeCardStyle>
+      <p>{joke}</p>
+    </JokeCardStyle>
+  </Detach>
 );
 
 export default JokeCard;
@@ -13,7 +20,7 @@ JokeCard.propTypes = {
   joke: PropTypes.string.isRequired
 };
 
-const JokeCardStyle = styled.div`
+export const JokeCardStyle = styled.div`
   margin-bottom: 1.4rem;
   box-shadow: 1px 1px 1px 0px #d2d2d2;
   border: 1px solid #d2d2d2;
@@ -22,6 +29,7 @@ const JokeCardStyle = styled.div`
   cursor: pointer;
   font-size: 1rem;
   font-weight: 500;
+  background-color: white;
   p {
     word-break: break-word;
     line-height: 1.4;
