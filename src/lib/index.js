9241029a5be6b33c919e7faddf016f28fc684caa
ahrbil <ahrbil.me@gmail.com>
2019-02-12 11:35:05 +0100

    Updating getJokes function to return one promise instead of array of them

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,7 @@
 //  function that takes length of  jokes we want and a category
 // and returns an array of promises that we can await to resolve
 // and getting our list of jokes
-export const getJokes = (count = 10, category = "") => {
+export const getJokes = async (count = 10, category = "") => {
   const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
   const jokesPromises = Array(count)
     .fill()
@@ -10,5 +10,6 @@ export const getJokes = (count = 10, category = "") => {
       const joke = await result.json();
       return joke;
     });
-  return jokesPromises;
+  const jokes = await Promise.all(jokesPromises);
+  return jokes;
 };
